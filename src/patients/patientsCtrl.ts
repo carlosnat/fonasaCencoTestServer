04e035d9a6f9ad4c8c7af3211fb977c81859a16e
Carlos Natera <carlosnatera@MacBook-Pro-de-Carlos.local>
2022-01-24 17:36:58 -0300

    refactor patient controller

--- src/patients/patientsCtrl.ts
+++ src/patients/patientsCtrl.ts
@@ -8,36 +8,43 @@ import {
 import utils from '../utils/utils';
 
 class PatientController {
+    async fetchRandomUser() {
+        const { data } = await Axios.get('https://randomuser.me/api/?results=1&inc=gender,name,dob,id,picture');
+        const [userData] = data.results;
+        return userData
+    }
+
+    buildPatientData(userData: any): TPatient {
+        const { name: { first, last }, gender } = userData;
+        const age = utils.generateRandomNumber(100)
+        return {
+            name: `${first} ${last}`,
+            age,
+            historyId: new Date().getTime().toString(),
+            gender
+        };
+    }
+
     async generate() {
         try {
-            const { data } = await Axios.get('https://randomuser.me/api/?results=1&inc=gender,name,dob,id,picture');
-            const [userData] = data.results;
-            const { name: { first, last }, gender } = userData;
-            const age = utils.generateRandomNumber(100)
-            const newPatientData = { 
-                name: `${first} ${last}`, 
-                age, 
-                historyId: new Date().getTime().toString(), 
-                gender
-            };
-            const newPatient = this.classifyPatient(newPatientData);
-            newPatient?.calculateExtraProp();
-            return newPatient;
+            const randomData = await this.fetchRandomUser()
+            const newPatientData = this.buildPatientData(randomData)
+            return this.classifyPatient(newPatientData);
         } catch (error) {
             return { error };
         }
     }
 
     classifyPatient(patientData: TPatient) {
-        const { age  } = patientData;
+        const { age } = patientData;
         let generatedPatient = undefined;
-        if(age <= 15) {
+        if (age <= 15) {
             generatedPatient = new ChildPatient(patientData);
         }
-        if(age >= 16 && age <= 40) {
+        if (age >= 16 && age <= 40) {
             generatedPatient = new YoungPatient(patientData);
         }
-        if(age >= 41) {
+        if (age >= 41) {
             generatedPatient = new ElderlyPatient(patientData);
         }
         return generatedPatient;
