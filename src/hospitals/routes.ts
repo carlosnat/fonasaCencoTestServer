f097d4d4ebec8be08ee902c8deb0e164c6f9ce3b
Carlos Natera <carlosnatera@MacBook-Pro-de-Carlos.local>
2022-01-25 14:46:53 -0300

    create hospital

--- src/hospitals/routes.ts
+++ src/hospitals/routes.ts
@@ -1,14 +1,25 @@
 import { Router } from "express";
+import { sequelize } from "../database/connection";
 import myHospital from "../hospitals/hospital";
 
 const router = Router()
 
-router.use('/report', async (req, res) => {
+router.get('/report', async (req, res) => {
     res.send({
         pediatria: myHospital.pediatria.length,
         general: myHospital.consultaGeneral.length,
         urgencia: myHospital.urgencia.length
     })
+})
+
+router.post('/', async(req, res) => {
+    const { name, address } = req.body
+    const newHospital = sequelize.models.Hospital.build({
+        name,
+        address
+    })
+    await newHospital.save()
+    res.send(newHospital)
 })
 
 export default router
